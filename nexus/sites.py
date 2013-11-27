c1d8d95ae7bfd04a524ad4c8b656a82f746c4b03
Stefano DAmico <skdamico@gmail.com>
2013-11-27 11:02:22 -0800

    Use python functools version of update_wrapper

--- nexus/sites.py
+++ nexus/sites.py
@@ -7,11 +7,12 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.template.loader import render_to_string
 from django.utils.datastructures import SortedDict
-from django.utils.functional import update_wrapper
 from django.utils.http import http_date
 from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import csrf_protect
 from django.views.static import was_modified_since
+
+from functools import update_wrapper
 
 from nexus import conf
 
